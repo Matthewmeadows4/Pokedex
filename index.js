6bfeba9c8e075b71a38d00ef5ebbdb780646b043
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-10-13 22:46:24 -0400

    test

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const api_url = 'https://pokeapi.co/api/v2/pokemon?limit=151&species'
+const api_url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+
 
 async function pokeapi() {
     const response = await fetch(api_url);
-    const data = await response.json();
+    const data= await response.json()
 
-    for (const name in data.results) {
-        let pokemon = data.results[name];
-        console.log(pokemon.name)        
-    }
-}
+    let array = []
+    for(let i = 1; i <= 151; i++);
+    var pokeName =(data.results.map(results => results.name))
+    console.log(array + pokeName);}
+
 pokeapi();
 
 
