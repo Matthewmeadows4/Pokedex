728a7309a8614b5eb9b4046322a24e16504f2109
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-10-19 12:18:56 -0400

    worked on getting specific objects from API call

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const api_url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+//const api_url = 'https://pokeapi.co/api/v2/pokemon/${i}'
+//const limit = '?limit=151'
 
+const pokedex = document.getElementById("pokedex");
 
-async function pokeapi() {
-    const response = await fetch(api_url);
-    const data= await response.json()
+function displayMon() {
+  //const promises = [];
+  const url = `https://pokeapi.co/api/v2/pokemon/`;
+  const input = document.getElementById("poke");
+  const pokeArray = [];
+  
+ pokeArray.push(fetch(url + input.value).then((res) => res.json()))
+    Promise.all(pokeArray).then(results => {
+        const pokemon = results.map(data => ({
+            na
+        })
+        )
+     
+        
+      } );
+    })
 
-    let array = []
-    for(let i = 1; i <= 151; i++);
-    var pokeName =(data.results.map(results => results.name))
-    console.log(array + pokeName);}
-
-pokeapi();
-
-
-
+}
