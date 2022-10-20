c2ed4e46b48f6c8cf8d7fce8b9c2bc066ba633fc
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-10-20 09:07:41 -0400

    got the correct objects to display

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 //const api_url = 'https://pokeapi.co/api/v2/pokemon/${i}'
 //const limit = '?limit=151'
-
-const pokedex = document.getElementById("pokedex");
-
+const pokeArray = []
+const url = `https://pokeapi.co/api/v2/pokemon/`;
+const input = document.getElementById("poke");
 function displayMon() {
-  const url = `https://pokeapi.co/api/v2/pokemon/`;
-  const input = document.getElementById("poke");
-  fetch(url + input.value)
-    .then((res) => res.json())
-    .then(results => {
-      document.getElementById('idMon').textContent = results.types[0].type.name
-    }
-    )}
+  //for (let i = 1; i < 150 ; i++)
+  pokeArray.push(fetch(url + input.value)
+    .then((res) => res.json()));
+
+    Promise.all(pokeArray).then(results => {
+    const pokemon = results.map(data => ({
+      name: data.name,
+      type: data.types.map (type => type.type.name).join(', ')
+    }));
+document.getElementById('name').textContent = (pokemon[0].name) ;
+document.getElementById('type').textContent = (pokemon[0].type);
+  })}
