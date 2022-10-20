a233264a2307722671058acf8be6208eb1ea6cb1
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-10-20 08:32:58 -0400

    changing approach

--- index.js
+++ index.js
@@ -4,20 +4,11 @@
 const pokedex = document.getElementById("pokedex");
 
 function displayMon() {
-  //const promises = [];
   const url = `https://pokeapi.co/api/v2/pokemon/`;
   const input = document.getElementById("poke");
-  const pokeArray = [];
-  
- pokeArray.push(fetch(url + input.value).then((res) => res.json()))
-    Promise.all(pokeArray).then(results => {
-        const pokemon = results.map(data => ({
-            na
-        })
-        )
-     
-        
-      } );
-    })
-
-}
+  fetch(url + input.value)
+    .then((res) => res.json())
+    .then(results => {
+      document.getElementById('idMon').textContent = results.types[0].type.name
+    }
+    )}
