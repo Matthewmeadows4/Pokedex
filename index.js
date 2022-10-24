c3e23662558314d0473de27a7f16b666196d0d9d
Matthew Meadows <Matthewmeadows4@gmail.com>
2022-10-24 12:04:07 -0400

    worked on suggestion functionality

--- index.js
+++ index.js
@@ -1,61 +1,73 @@
 //const api_url = 'https://pokeapi.co/api/v2/pokemon/${i}'
 //const limit = '?limit=151'
-const pokeArray = []
+const pokeArray = [];
 const url = `https://pokeapi.co/api/v2/pokemon/`;
-const limit = `?limit=1154`
-const input = document.getElementById("poke")
-
+const limit = `?limit=1154`;
+const input = document.getElementById("poke");
 
 function displayMon() {
-  pokeArray.push(fetch(url + input.value.toLowerCase())
-    .then((res) => res.json()));
-    Promise.all(pokeArray).then(results => {
-    const pokemon = results.map(data => ({
+  pokeArray.push(
+    fetch(url + input.value.toLowerCase()).then((res) => res.json())
+  );
+  Promise.all(pokeArray).then((results) => {
+    const pokemon = results.map((data) => ({
       id: data.id,
       name: data.name,
-      type: data.types.map (type => type.type.name).join(', '),
+      type: data.types.map((type) => type.type.name).join(", "),
       image: data.sprites["front_default"],
     }));
-document.getElementById('name').textContent = (pokemon[0].name);
-document.getElementById('type').textContent = (pokemon[0].type);
-document.getElementById('num').textContent = (pokemon[0].id);
-  })};
+    document.getElementById("name").textContent = pokemon[0].name;
+    document.getElementById("type").textContent = pokemon[0].type;
+    document.getElementById("num").textContent = pokemon[0].id;
+  });
+}
 
-const search = document.getElementById('poke');
-const matchList = document.getElementById('match-list')
-const searchMon = async searchText => {
+const search = document.getElementById("poke");
+const pokeListElement = document.querySelector("#poke-list");
+const searchMon = async (searchText) => {
   const res = await fetch(url + limit);
-  const mons = await res.json()
+  const mons = await res.json();
 
-  let matches = mons.results.filter(mon => {
-    const regex =  new RegExp(`^${searchText}`, 'gi');
-    return mon.name.match(regex)
+  let matches = mons.results.filter((mon) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return mon.name.match(regex);
   });
 
-  if (searchText.length === 0){
-  matches = 0
+  if (searchText.length === 0) {
+    matches = 0;
   }
-  outputHTML(matches);
+  loadPoke(matches, pokeListElement);
+  // outputHTML(matches);
 };
 
-const outputHTML = matches => {
-  if (matches.length > 0){
-    const html = matches.map( match =>
-      `<div class="results">
-      <ul>${match.name}<ul><span class="pokeSpan" </span></h4>
-      </div>`
-  )
-  .join ('');
-
-  matchList.innerHTML = html.toUpperCase()
-    }
-  };
-
-  
-poke.addEventListener('input', () => searchMon(poke.value))
-
-  function reset(){
-    window.location.reload("Refresh")
+function loadPoke(mons, element) {
+  if (mons) {
+    element.innerHTML = "";
+    let innerElement = "";
+    mons.forEach((mons) => {
+      innerElement += `<li><a href = "#">${mons.name}</a></li>`;
+    });
+    element.innerHTML = innerElement;
   }
 
+}
 
+
+poke.addEventListener("input", () => searchMon(poke.value));
+
+
+function getEventTarget(e) {
+  e = e || window.event;
+  return e.target || e.srcElement; 
+}
+
+const li = document.getElementById('poke-list');
+li.onclick = function(event) {
+  const target = getEventTarget(event);
+  console.log(target.innerHTML);
+};
+
+
+function reset() {
+  window.location.reload("Refresh");
+}
